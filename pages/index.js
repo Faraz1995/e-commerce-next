c821a91cceee19cc5d6ea631f23a8e65eaccb940
FarazMehraien <farazmehraien95@gmail.com>
2021-11-23 17:08:26 +0330

    update text

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to shop dude</h1>
+        <h1 className={styles.title}>Welcome to the best shop dude</h1>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
